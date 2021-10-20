21f32d2e962cada09a15a36e8c854a9cb2e592f7
emily <emil7809@gmail.com>
2021-10-20 15:27:07 +0200

    first

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", start);
+
+let personality = false;
+
+function start() {
+    console.log("start");
+    document.querySelector("#pro_vs_personality").addEventListener("click", proVsPer);
+}
+
+function proVsPer() {
+
+    if (personality === false) {
+        personality = true;
+        document.querySelector(".per").classList.add("show");
+        document.querySelector("#pro_vs_personality").style.backgroundColor = "pink";
+    } else {
+        personality = false;
+        document.querySelector(".per").classList.remove("show"); 
+        document.querySelector("#pro_vs_personality").style.backgroundColor = "white"; 
+    }
+
+    
+}
